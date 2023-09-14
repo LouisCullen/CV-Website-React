0743c5fef4f74c89fc86d2ec3584606a79f53a3f
Louis Cullen <louis.cullen@student.manchester.ac.uk>
2023-09-14 20:11:02 +0100

    better skill items

--- cv-website/src/components/SkillItem.tsx
+++ cv-website/src/components/SkillItem.tsx
@@ -7,84 +7,6 @@ interface popover {
     uses: string[];
 }
 
-interface internalProps {
-    icon: JSX.Element;
-    popover: popover;
-    setPopover: any;
-    height: string;
-    width: string;
-    padding: string;
-    iconCover: string;
-    justifyContent: string;
-    mobile: boolean;
-}
-
-const SkillItemInternals = ({
-    icon,
-    popover,
-    setPopover,
-    height,
-    width,
-    padding,
-    iconCover,
-    justifyContent,
-    mobile
-}: internalProps) => {
-
-    return (
-        <div 
-            style={{ 
-                display: "flex", 
-                position: "relative", 
-                maxHeight: height,
-                maxWidth: width,
-                transitionDuration: "1s",
-                overflow: "hidden",
-                flexDirection: mobile ? "row" : "column",
-                width: "100%",
-                margin: mobile ? 0 : padding,
-                justifyContent: justifyContent
-            }}
-        >
-            <div
-                style={{
-                    position: "relative",
-                    flexShrink: 0,
-                    padding: (iconCover === "40%") ? "5%" : 0,
-                    width: iconCover,
-                    color: "#bdc3c7"
-                }}
-                onClick={() => {
-                    setPopover(popover.name);
-                }}
-                // onMouseLeave={() => {
-                //     setPopover(null);
-                // }}
-            >
-                {icon}
-            </div>
-            <div 
-                style={{ 
-                    // width: "min(300px,27vw)", 
-                    display: "flex", 
-                    alignItems: "center", 
-                    flexDirection: "column",
-                    paddingTop: mobile ? 0 : "5%",
-                    justifyContent: "center",
-                    marginLeft: mobile ? "2%" : 0
-                }}
-            >
-                <h2 className="skill-name" style={{ fontWeight: "bold", color: "#9204e8 !important" }}>{popover.name}</h2>
-                <ul style={{ textAlign: "center", listStylePosition: "inside", padding: 0, margin: 0, overflow: "hidden", whiteSpace: "nowrap" }}>
-                    {popover.uses.map((use, index) => 
-                        <li key={index}>{use}</li>
-                    )}
-                </ul>
-            </div>
-        </div>
-    )
-}
-
 interface props {
     icon: JSX.Element;
     popover: popover;
@@ -132,7 +54,7 @@ const SkillItem = ({
             setIconCover("100%");
             setHeight("min(100px,10vw)");
             setWidth("min(100px,10vw)");
-            setPadding("max(15px, 1.5vw)");
+            setPadding("max(15px, 2vw)");
             setJustifyContent("start");
             // setTimeout(() => {
             //     setOrder(0);
@@ -141,43 +63,71 @@ const SkillItem = ({
     }, [globalPopover, mobile, popover.name]);
 
     return (
-        mobile ? (
             <motion.div
             variants={variants}
-            style={{
+            style={mobile ? {
                 maxHeight: height,
                 maxWidth: width,
                 width: "100%",
                 display: "flex",
                 justifyContent: "center",
                 margin: padding,
+            } : {
+                margin: padding
             }}
             >
-                <SkillItemInternals
-                    icon={icon}
-                    popover={popover}
-                    setPopover={setPopover}
-                    height={height}
-                    width={width}
-                    padding={padding}
-                    iconCover={iconCover}
-                    justifyContent={justifyContent}
-                    mobile={mobile}
-                />
+                <div 
+                    style={{ 
+                        display: "flex", 
+                        position: "relative", 
+                        maxHeight: height,
+                        maxWidth: width,
+                        transitionDuration: "1s",
+                        overflow: "hidden",
+                        flexDirection: mobile ? "row" : "column",
+                        width: "100%",
+                        justifyContent: justifyContent,
+                    }}
+                >
+                    <div
+                        style={{
+                            position: "relative",
+                            flexShrink: 0,
+                            padding: (iconCover === "40%") ? "5%" : 0,
+                            width: iconCover,
+                            color: "#bdc3c7",
+                            height: mobile ? height : "100%",
+                            // width: width
+                        }}
+                        onClick={() => {
+                            setPopover(popover.name);
+                        }}
+                        // onMouseLeave={() => {
+                        //     setPopover(null);
+                        // }}
+                    >
+                        {icon}
+                    </div>
+                    <div 
+                        style={{ 
+                            // width: "min(300px,27vw)", 
+                            display: "flex", 
+                            alignItems: "center", 
+                            flexDirection: "column",
+                            paddingTop: mobile ? 0 : "5%",
+                            justifyContent: "center",
+                            marginLeft: mobile ? "2%" : 0
+                        }}
+                    >
+                        <h2 className="skill-name" style={{ fontWeight: "bold", color: "#9204e8 !important" }}>{popover.name}</h2>
+                        <ul style={{ textAlign: "center", listStylePosition: "inside", padding: 0, margin: 0, overflow: "hidden", whiteSpace: "nowrap" }}>
+                            {popover.uses.map((use, index) => 
+                                <li key={index}>{use}</li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
             </motion.div>
-        ) : (
-            <SkillItemInternals
-                icon={icon}
-                popover={popover}
-                setPopover={setPopover}
-                height={height}
-                width={width}
-                padding={padding}
-                iconCover={iconCover}
-                justifyContent={justifyContent}
-                mobile={mobile}
-            />
-        )
 
     )
 }
